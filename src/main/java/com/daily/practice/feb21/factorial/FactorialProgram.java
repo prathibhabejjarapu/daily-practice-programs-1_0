28355fc525c35683030abae75e227ef3f4f1855b
prathibhabejjarapu <prathibhabejjarapu@gmail.com>
2024-02-21 23:43:45 -0600

    factorial2

--- src/main/java/com/daily/practice/feb21/factorial/FactorialProgram.java
+++ src/main/java/com/daily/practice/feb21/factorial/FactorialProgram.java
@@ -1,7 +1,7 @@
 package com.daily.practice.feb21.factorial;
 
 public class FactorialProgram {
-	int  result = 1;
+	long  result = 1;
 
 	public long fact(int number) {
 		for (int i = number; i >= 2; i--) {
